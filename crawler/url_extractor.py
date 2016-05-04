1929d24b32b6ed6d956f2317d8b09ce9683abe40
moreD <tigermoreD@gmail.com>
2016-05-04 05:56:39 -0400

    add lib filter

--- crawler/url_extractor.py
+++ crawler/url_extractor.py
@@ -4,6 +4,7 @@ from urlparse import urljoin
 import re
 
 filter_words = [
+	'[\S]*lib.tsinghua.edu.cn[\S]*',
     '[\S]*166.111.120.[\S]*',
     #'.*(?i)\.(mso|tar|txt|asx|asf|bz2|mpe?g|MPE?G|tiff?|gif|GIF|png|PNG|ico|ICO|css|sit|eps|wmf|zip|pptx?|xlsx?|gz|rpm|tgz|mov|MOV|exe|jpe?g|JPE?G|bmp|BMP|docx?|DOCX?|pdf|PDF|rar|RAR|jar|JAR|ZIP|zip|gz|GZ|wma|WMA|rm|RM|rmvb|RMVB|avi|AVI|swf|SWF|mp3|MP3|wmv|WMV|ps|PS)$',
     '.*(?i)\.(mso|tar|txt|asx|asf|bz2|mpe?g|MPE?G|tiff?|gif|GIF|png|PNG|ico|ICO|css|sit|eps|wmf|zip|gz|rpm|tgz|mov|MOV|exe|jpe?g|JPE?G|bmp|BMP|rar|RAR|jar|JAR|ZIP|zip|gz|GZ|wma|WMA|rm|RM|rmvb|RMVB|avi|AVI|swf|SWF|mp3|MP3|wmv|WMV|ps|PS)$',
